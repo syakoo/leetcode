9409b3d8615dc083b28ea741cb70829325f55d27
syakoo <nichijou4009@gmail.com>
2021-07-16 19:34:44 +0900

    feat: add product().

--- libs/itertools.ts
+++ libs/itertools.ts
@@ -39,4 +39,30 @@ function* combinations<T>(iterable: Iterable<T>, r: number) {
   }
 }
 
-export { accumulate, combinations }
+// __________
+//
+function* product<T>(iterables: Iterable<T>[], repeat = 1) {
+  const pools = iterables.map((iter) => [...iter])
+  const lens = pools.map((pool) => pool.length)
+  const indices = [...new Array(pools.length * repeat)].fill(0)
+  const n = indices.length
+  const result = indices.map((v, i) => pools[Math.floor(i / repeat)][v])
+  yield result
+  while (true) {
+    let i
+    for (i = n - 1; i >= 0; i--) {
+      if (indices[i] === lens[Math.floor(i / repeat)] - 1) {
+        indices[i] = 0
+        result[i] = pools[Math.floor(i / repeat)][0]
+      } else {
+        break
+      }
+    }
+    if (i === -1) return
+    indices[i]++
+    result[i] = pools[Math.floor(i / repeat)][indices[i]]
+    yield [...result]
+  }
+}
+
+export { accumulate, combinations, product }
