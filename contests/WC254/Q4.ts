e08519a94f6c502ade2fa7df85117c63f87cfcb5
syakoo <nichijou4009@gmail.com>
2021-08-18 21:57:06 +0900

    feat: add Q4 of WC254.

--- contests/WC254/Q4.ts
+++ contests/WC254/Q4.ts
@@ -1,14 +1,18 @@
-function latestDayToCross(row: number, col: number, cells: number[][]): number {
-  function check(waters: Set<string>) {
-    const used = [...Array(row)].map(() => [...Array(col)].fill(0))
-    const starts = [...Array(col)]
-      .map((_, i): [number, number] => [0, i])
+type Coeff = [number, number]
+
+function latestDayToCross(row: number, col: number, cells: Coeff[]): number {
+  function isConnected(waters: Set<string>) {
+    const visited = [...Array(row)].map(() => [...Array(col)].fill(false))
+    const tops: Coeff[] = [...Array(col)]
+      .map((_, i): Coeff => [0, i])
       .filter((_, i) => !waters.has(`0,${i}`))
-    const stack: [number, number][] = starts
-    starts.forEach(([h, w]) => (used[h][w] = 1))
+    const stack: Coeff[] = tops
+    tops.forEach(([h, w]) => {
+      visited[h][w] = true
+    })
 
     while (stack.length > 0) {
-      const [h, w] = stack.pop() as unknown as number[]
+      const [h, w] = stack.pop() as unknown as Coeff
       for (const [dh, dw] of [
         [-1, 0],
         [1, 0],
@@ -21,33 +25,37 @@ function latestDayToCross(row: number, col: number, cells: number[][]): number {
           0 <= nh &&
           nh < row &&
           0 <= nw &&
-          nw <= col &&
-          used[nh][nw] === 0 &&
+          nw < col &&
+          !visited[nh][nw] &&
           !waters.has(`${nh},${nw}`)
         ) {
-          used[nh][nw] = 1
+          visited[nh][nw] = true
           stack.push([nh, nw])
+
           if (nh === row - 1) {
             return true
           }
         }
       }
     }
+
     return false
   }
 
   let [l, r] = [0, cells.length]
   while (r - l > 1) {
     const m = Math.floor((r + l) / 2)
-    const ws = new Set<string>()
+    const waters = new Set<string>()
     for (let i = 0; i < m; i++) {
-      ws.add(`${cells[i][0] - 1},${cells[i][1] - 1}`)
+      waters.add(`${cells[i][0] - 1},${cells[i][1] - 1}`)
     }
-    if (check(ws)) {
+
+    if (isConnected(waters)) {
       l = m
     } else {
       r = m
     }
   }
+
   return l
 }
